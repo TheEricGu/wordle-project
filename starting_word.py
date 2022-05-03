1090211afcdd064cbab25635eaca337b3e2e2938
Eric Gu <efg37@cornell.edu>
2022-05-03 01:58:16 -0400

    completed COPOD but still some bugs

--- starting_word.py
+++ starting_word.py
@@ -2,26 +2,64 @@ from collections import Counter
 import pandas as pd
 import string
 from tqdm import tqdm
+import re
+from pyod.models.copod import COPOD
 
 def import_words(file_name):
     with open(file_name) as f:
         words = f.read().splitlines()
     return words
 
-words_accepted = import_words("words_accepted.txt")
-words_solutions = import_words("words_solutions.txt")
+def starting_word(words_accepted, words_solutions, raw_address):
+    if raw_address:
+        words_accepted = import_words(words_accepted)
+        words_solutions = import_words(words_solutions)
+    print("Accepted words:", len(words_accepted))
+    print("Solution words:", len(words_solutions))
 
-accepted_word_string = ''.join(words_accepted)
-letter_counts = dict(Counter(accepted_word_string))
+    accepted_word_string = ''.join(words_accepted)
+    letter_counts = dict(Counter(accepted_word_string))
+    print("Cumulative letter counts:", letter_counts)
 
-letter_frequencies = {letter:count/len(words_accepted) 
-for letter,count in letter_counts.items()}
+    letter_frequencies = {letter:count/len(words_accepted) 
+    for letter,count in letter_counts.items()}
+    print("Cumulative letter frequencies:", letter_frequencies)
 
-letter_frequencies = pd.DataFrame({'Letter':list(letter_frequencies.keys()),
-'Frequency':list(letter_frequencies.values())}).sort_values('Frequency',
-ascending=False)
+    letter_frequencies = pd.DataFrame({'Letter':list(letter_frequencies.keys()),
+    'Frequency':list(letter_frequencies.values())}).sort_values('Frequency',
+    ascending=False)
 
-letter_positions = []
-for letter in list(string.ascii_lowercase):
-    for position in range(5):
-        letter_positions.append(f'{letter}:{position}')
+    letter_positions = []
+    for letter in list(string.ascii_lowercase):
+        for position in range(5):
+            letter_positions.append(f'{letter}:{position}')
+
+    letter_positions_df = pd.DataFrame()
+    for word in tqdm(words_solutions): 
+        letter_position_str = ''.join([f"{letter}:{position}" for position, letter in enumerate(word)])
+        letter_position_counter = {}
+        for letter_position in letter_positions:
+            letter_position_counter[letter_position] = len(re.findall(letter_position, letter_position_str))
+
+        temp_letter_positions_df = pd.DataFrame(letter_position_counter, index=[word])
+        letter_positions_df = pd.concat([letter_positions_df, temp_letter_positions_df])
+    print("Letter position dataframe:")
+    print(letter_positions_df.head(5))
+
+    print("Shape of letter position dataframe:", letter_positions_df.shape)
+    for column in letter_positions_df.columns:
+        if letter_positions_df[column].sum() == 0:
+            letter_positions_df.drop(column, axis=1, inplace=True)
+    print("Shape after dropping columns with only zeros:", letter_positions_df.shape)
+            
+    print("Fitting COPOD model...")
+    copod_model = COPOD(contamination=0.01)
+    copod_model.fit(letter_positions_df)
+
+    print("Generating decision scores...")
+    letter_positions_df['score'] = copod_model.decision_scores_
+    letter_positions_df.sort_values('score',inplace=True)
+
+    print("Top 10 words with highest decision scores:")
+    letter_positions_df['rank'] = range(1,len(letter_positions_df)+1)
+    print(letter_positions_df.head(10)[['score','rank']])
