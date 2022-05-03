11972f398e92161f291c51b0136149ce5551881a
Eric <efg37@cornell.edu>
2022-05-03 13:42:41 -0400

    reversed condition order

--- wordle_solver.py
+++ wordle_solver.py
@@ -16,11 +16,9 @@ def yellow_checker(yellow, word, guess):
             return False
         if guess_counts[y[0]] > word_counts[y[0]]:
             return False
-
     for item in yellow:
         if word[item[1]] == item[0]:
             return False
-            
     return True
 
 def green_checker(green, word):
@@ -35,7 +33,6 @@ def prune_words(accepted, solutions, guess, colors):
     black = []
     yellow = []
     green = []
-
     for i in range(len(guess)):
         if colors[i] == 'b':
             black.append(guess[i])
@@ -46,25 +43,21 @@ def prune_words(accepted, solutions, guess, colors):
         else:
             print("Invalid color input")
             return
-
     # print(black)
     # print(yellow)
     # print(green)
 
     for word in tqdm(accepted):
-        if black_checker(black, word):
+        if green_checker(green, word):
             if yellow_checker(yellow, word, guess) or len(yellow) == 0:
-                if green_checker(green, word):
+                if black_checker(black, word):
                     new_accepted.append(word)
-    # print("New accepted:", new_accepted)
 
     for word in tqdm(solutions):
         if black_checker(black, word):
             if yellow_checker(yellow, word, guess):
                 if green_checker(green, word):
                     new_solutions.append(word)
-    # print("New solutions:", new_solutions)
-        
     return new_accepted, new_solutions
 
 def solver(accepted, solutions):
@@ -72,6 +65,9 @@ def solver(accepted, solutions):
         guess = input("Enter your guess: ")
         if len(guess) != 5:
             print("Guess length should be 5")
+            return
+        if guess not in accepted:
+            print("Not a valid guess")
             return
         colors = input("Enter your colors: ")
         if colors == "ggggg":
@@ -85,7 +81,7 @@ def solver(accepted, solutions):
 
 print("Welcome to Worldle Solver!")
 print("Generating the top 10 best guesses for your starting word...")
-starting_word("words_accepted.txt", "words_solutions.txt", True)
+# starting_word("words_accepted.txt", "words_solutions.txt", True)
 accepted = import_words("words_accepted.txt") 
 solutions = import_words("words_solutions.txt") 
 solver(accepted, solutions)
